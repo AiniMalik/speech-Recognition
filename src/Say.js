19c72daebab6ad65c82b4754b724b64f62f95afe
AiniMalik <ainimalik9465@gmail.com>
2023-01-12 20:15:57 +0500

    type

--- src/Say.js
+++ src/Say.js
@@ -8,7 +8,7 @@ function Say() {
 
       <audio controls>
         <source
-          src={`http://208.109.188.242:5003/api/tts?voice=en-us/southern_english_female-glow_tts&text=${"Helo How are you doing great!"}&vocoder=hifi_gan%2Funiversal_large&denoiserStrength=0.002&noiseScale=0.667&lengthScale=0.85&ssml=false`}
+          src={`https://208.109.188.242:5003/api/tts?voice=en-us/southern_english_female-glow_tts&text=${"Helo How are you doing great!"}&vocoder=hifi_gan%2Funiversal_large&denoiserStrength=0.002&noiseScale=0.667&lengthScale=0.85&ssml=false`}
           type="audio/wav"
         />
         Your browser does not support the audio element.
