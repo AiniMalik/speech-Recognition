362c005967f3c5a003ac39c10865df3dad8c72d2
AiniMalik <ainimalik9465@gmail.com>
2023-01-12 20:14:50 +0500

    change type

--- src/Say.js
+++ src/Say.js
@@ -9,7 +9,7 @@ function Say() {
       <audio controls>
         <source
           src={`http://208.109.188.242:5003/api/tts?voice=en-us/southern_english_female-glow_tts&text=${"Helo How are you doing great!"}&vocoder=hifi_gan%2Funiversal_large&denoiserStrength=0.002&noiseScale=0.667&lengthScale=0.85&ssml=false`}
-          type="audio/ogg"
+          type="audio/wav"
         />
         Your browser does not support the audio element.
       </audio>
